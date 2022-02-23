c7b84c114a88a3278723718e6c9f3be31577603c
Derrick Park <derrick8689@gmail.com>
2022-02-23 09:28:11 -0800

    Add lab1 sample solution

---
+++ src/labs/lab1/Driver.java
@@ -0,0 +1,16 @@
+package labs.lab1;
+
+public class Driver {
+    public static void main(String[] args) {
+        Model susan = new Model("Susan", "Smith",
+                70, 120,true,false);
+
+        Model tiger = new Model("Tiger",
+                "Woods", 72, 190, true, false);
+
+        susan.displayModelDetails();
+        System.out.println();
+        tiger.displayModelDetails();
+
+    }
+}
